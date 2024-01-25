08b2950d956e3493204373a36838ff5f2f7617af
mwandell-hilbgroup <mwandell@hilbgroup.com>
2024-01-25 09:12:04 -0500

    Updated MSI Command

--- AppVoumeAgentInstall/AppVolumesInstall.ps1
+++ AppVoumeAgentInstall/AppVolumesInstall.ps1
@@ -6,7 +6,7 @@ try {
     Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
 
     # Install the MSI file
-    Start-Process msiexec -ArgumentList "/i $downloadPath /qn MANAGER_ADDR=appvolumemanager.hilbgroup.com MANAGER_PORT=443 EnforceSSLCertificateValidation=0" -Wait
+    Start-Process msiexec -ArgumentList "/i $downloadPath /quiet /norestart MANAGER_ADDR=appvolumemanager.hilbgroup.com MANAGER_PORT=443 EnforceSSLCertificateValidation=0" -Wait
 }
 catch {
     Write-Host "Error occurred during download or installation: $_"
