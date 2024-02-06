23a9e52bd33f5654fa65878248115c4ff8aba08f
mwandell-hilbgroup <mwandell@hilbgroup.com>
2024-02-06 13:35:52 -0500

    Add installation of RDM along with FileZilla

--- AVDImageInstallation/AdminDesktopInstall.ps1
+++ AVDImageInstallation/AdminDesktopInstall.ps1
@@ -3,3 +3,4 @@ Enable-WindowsOptionalFeature -Online -FeatureName RSAT-ADDS -All
 Enable-WindowsOptionalFeature -Online -FeatureName RSAT-DNS-Server -All
 
 choco install filezilla -y -Force
+choco install rdm -y -Force
