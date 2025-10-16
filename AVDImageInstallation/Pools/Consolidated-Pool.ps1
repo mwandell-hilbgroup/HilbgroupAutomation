827090ced52f56895773c61de726c7e6ce7f59c7
wandellms <Wandellms@me.com>
2025-10-16 14:03:14 -0400

    Add installation functions for Qualys, NetSkope, SentinelOne, and Workday Office Connect; update installation sequence

--- AVDImageInstallation/Pools/Consolidated-Pool.ps1
+++ AVDImageInstallation/Pools/Consolidated-Pool.ps1
@@ -314,6 +314,39 @@ function Install-Nasa {
     }
 }
 
+
+function Install-Qualys {
+    $ActivationID = "fa0969cf-7264-45bf-be34-52632dcca96f"
+    $CustomerID = "0fc184d4-664e-73f8-82c6-ef818818f7ca"
+    $WebURI = "https://qagpublic.qg3.apps.qualys.com/CloudAgent/"
+    $Arguments = "CustomerID={" + $CustomerID + "} ActivationId={" + $ActivationID + "} WebServiceUri=" + $WebURI
+    $DownloadURL = "https://eusthginfrastructure.blob.core.windows.net/thg-software-deploy/CloudAgent_x64.msi"
+
+    Invoke-DownloadAndStartProcess -DownloadURL $DownloadURL -Arguments $Arguments
+}
+
+function Install-NetSkope {
+    $Arguments = 'token=e6cFeDnQ3Kw38C54KNiy host=addon-hilbgroup.goskope.com mode=peruserconfig autoupdate=on enrollauthtoken=1ed082ef63c3e4a434b7118a3de92876 enrollencryptiontoken=2e48fb92442ba1677a33772ea62f5268 /qn /l*v C:\hilb\nscinstall.log'
+    $DownloadURL = 'https://eusthginfrastructure.blob.core.windows.net/thg-software-deploy/NSClient.msi'
+
+    Invoke-DownloadAndStartProcess -DownloadURL $DownloadURL -FileName 'NSClient.msi' -Arguments $arguments
+}
+
+function Install-SentinelOne {
+
+    $S1URL64 = "https://eusthginfrastructure.blob.core.windows.net/thg-software-deploy/S1.msi"
+    $S1File64 = "S1.msi"
+    $Arguments = '/q /norestart SITE_TOKEN="eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS1jc3MtczEwMS5zZW50aW5lbG9uZS5uZXQiLCAic2l0ZV9rZXkiOiAiYTljNDJjYzYxNDUzM2NkNyJ9"'
+
+    Invoke-DownloadAndStartProcess -DownloadURL $S1URL64 -FileName $S1File64 -Arguments $Arguments
+}
+
+function Install-WorkdayOfficeConnect {
+    $url = "https://clickonce.adaptiveinsights.com/officeconnect/latest/OfficeConnectMachineSetup.exe"
+
+    Invoke-DownloadAndStartProcess -DownloadURL $url -FileName "OfficeConnectMachineSetup.exe" -Arguments "/quiet"
+}
+
 Write-Host "Installing Chocolatey Packages"
 Install-ChocoPackages -Packages @('powerbi')
 
@@ -343,3 +376,11 @@ Install-PSQL
 
 Write-Host "Installing NASA"
 Install-Nasa
+
+Install-Qualys
+
+Install-NetSkope
+
+Install-SentinelOne
+
+Install-WorkdayOfficeConnect
