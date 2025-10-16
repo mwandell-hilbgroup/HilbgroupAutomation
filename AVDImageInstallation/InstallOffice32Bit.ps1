5bd4c8696f8ebed74123647d82b452889ce10ea9
wandellms <Wandellms@me.com>
2025-10-16 10:19:33 -0400

    Update Office installation script: add language settings for Visio and exclude apps, fix company name format, and streamline error handling.

--- AVDImageInstallation/InstallOffice32Bit.ps1
+++ AVDImageInstallation/InstallOffice32Bit.ps1
@@ -12,8 +12,13 @@ $configXML = @"
     <ExcludeApp ID="Access" />
   </Product>
   <Product ID="AccessRuntimeRetail">
-      <Language ID="en-us" />
-    </Product>
+    <Language ID="en-us" />
+  </Product>
+  <Product ID="VisioProRetail">
+    <Language ID="en-us" />
+    <ExcludeApp ID="Groove" />
+    <ExcludeApp ID="Lync" />
+  </Product>
 </Add>
 <Property Name="SharedComputerLicensing" Value="1" />
 <Property Name="FORCEAPPSHUTDOWN" Value="FALSE" />
@@ -22,7 +27,7 @@ $configXML = @"
 <Updates Enabled="TRUE" />
 <RemoveMSI />
 <AppSettings>
-  <Setup Name="Company" Value="The Hilb Group" />
+  <Setup Name="Company" Value="The Hilb Group, LLC." />
   <User Key="software\microsoft\office\16.0\common\graphics" Name="disablehardwareacceleration" Value="1" Type="REG_DWORD" App="office16" Id="L_DoNotUseHardwareAcceleration" />
   <User Key="software\microsoft\office\16.0\excel\options" Name="defaultformat" Value="51" Type="REG_DWORD" App="excel16" Id="L_SaveExcelfilesas" />
   <User Key="software\microsoft\office\16.0\powerpoint\options" Name="defaultformat" Value="27" Type="REG_DWORD" App="ppt16" Id="L_SavePowerPointfilesas" />
@@ -32,29 +37,16 @@ $configXML = @"
 </Configuration>
 "@
 
-function Get-ODTURL {
-  
-    [String]$MSWebPage = Invoke-RestMethod 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117'
-  
-    $MSWebPage | ForEach-Object {
-        if ($_ -match 'url=(https://.*officedeploymenttool.*\.exe)') {
-            $matches[1]
-        }
-    }
-  
-}
-  
 if (-Not(Test-Path $OfficeInstallDownloadPath )) {
-    New-Item -Path $OfficeInstallDownloadPath -ItemType Directory | Out-Null
+  New-Item -Path $OfficeInstallDownloadPath -ItemType Directory | Out-Null
 }
 
-$ODTInstallLink = Get-ODTURL
+$ODTInstallLink = "https://download.microsoft.com/download/6c1eeb25-cf8b-41d9-8d0d-cc1dbc032140/officedeploymenttool_19231-20156.exe"
 
 Write-Verbose 'Downloading the Office Deployment Tool...'
 try {
   Invoke-WebRequest -Uri $ODTInstallLink -OutFile "$OfficeInstallDownloadPath\ODTSetup.exe"
-}
-catch {
+} catch {
   Write-Warning 'There was an error downloading the Office Deployment Tool.'
   Write-Warning 'Please verify the below link is valid:'
   Write-Warning $ODTInstallLink
@@ -63,45 +55,42 @@ catch {
 
 #Run the Office Deployment Tool setup
 try {
-    Write-Verbose 'Running the Office Deployment Tool...'
-    Start-Process "$OfficeInstallDownloadPath\ODTSetup.exe" -ArgumentList "/quiet /extract:$OfficeInstallDownloadPath" -Wait
-  }
-  catch {
-    Write-Warning 'Error running the Office Deployment Tool. The error is below:'
-    Write-Warning $_
-  }
+  Write-Verbose 'Running the Office Deployment Tool...'
+  Start-Process "$OfficeInstallDownloadPath\ODTSetup.exe" -ArgumentList "/quiet /extract:$OfficeInstallDownloadPath" -Wait
+} catch {
+  Write-Warning 'Error running the Office Deployment Tool. The error is below:'
+  Write-Warning $_
+}
 
 $configXML | Out-File $ConfigPath
 
 #Run the O365 install
 try {
-    Write-Verbose 'Downloading and installing Microsoft 365'
-    Start-Process "$OfficeInstallDownloadPath\Setup.exe" -ArgumentList "/configure $ConfigPath" -Wait -PassThru | Out-Null
-  }
-  catch {
-    Write-Warning 'Error running the Office install. The error is below:'
-    Write-Warning $_
-  }
+  Write-Verbose 'Downloading and installing Microsoft 365'
+  Start-Process "$OfficeInstallDownloadPath\Setup.exe" -ArgumentList "/configure $ConfigPath" -Wait -PassThru | Out-Null
+} catch {
+  Write-Warning 'Error running the Office install. The error is below:'
+  Write-Warning $_
+}
 
-  #Check if Office 365 suite was installed correctly.
+#Check if Office 365 suite was installed correctly.
 $RegLocations = @('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
-'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+  'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
 )
 
 $OfficeInstalled = $False
 foreach ($Key in (Get-ChildItem $RegLocations) ) {
-if ($Key.GetValue('DisplayName') -like '*Microsoft 365*') {
-  $OfficeVersionInstalled = $Key.GetValue('DisplayName')
-  $OfficeInstalled = $True
-}
+  if ($Key.GetValue('DisplayName') -like '*Microsoft 365*') {
+    $OfficeVersionInstalled = $Key.GetValue('DisplayName')
+    $OfficeInstalled = $True
+  }
 }
 Remove-Item -Path $OfficeInstallDownloadPath -Force -Recurse
 
 if ($OfficeInstalled) {
-Write-Verbose "$($OfficeVersionInstalled) installed successfully!"
-Exit 0
-}
-else {
-Write-Warning 'Microsoft 365 was not detected after the install ran'
-Exit 1
+  Write-Verbose "$($OfficeVersionInstalled) installed successfully!"
+  Exit 0
+} else {
+  Write-Warning 'Microsoft 365 was not detected after the install ran'
+  Exit 1
 }
